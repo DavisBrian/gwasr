d75343361e91a427baf20af8e5a70ae3dc4c0609
Brian Davis <brian.davis281@gmail.com>
2015-07-30 15:02:53 -0500

    gwas_list class stub

---
+++ R/gwas_list.R
@@ -0,0 +1,12 @@
+#' @export
+gwas_list <- function(geno, pheno, snpi) {
+    cmn <- Intersect(geno = geno, pheno = pheno, snpinfo = snpi)
+    
+    data <-     list(geno = reduce(geno, cmn), pheno = reduce(pheno, cmn), snpinfo = reduce(snpi, cmn, get_chrcol(snpi)))
+    structure(
+      data,
+      subjects = cmn$subjects,
+      snps = cmn$snps,
+      class = c("gwas_list")
+    )
+}
